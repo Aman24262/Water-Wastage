c23c1341c2be68692626c70704e2559b7e2c6adf
Aman Kumar <152580479+Aman24262@users.noreply.github.com>
2024-04-20 10:27:50 +0530

    Add files via upload

---
+++ facts.js
@@ -0,0 +1,99 @@
+var aa = document.querySelector("#aa");
+aa.addEventListener("mouseenter", function(){
+    aa.style.backgroundColor = "black"
+    aa.style.color = "mediumspringgreen"
+})
+aa.addEventListener("mouseleave", function(){
+    aa.style.backgroundColor = "white"
+    aa.style.color = "black"
+})
+
+var aa1 = document.querySelector("#aa1");
+aa1.addEventListener("mouseenter", function(){
+    aa1.style.backgroundColor = "black"
+    aa1.style.color = "yellow"
+})
+aa1.addEventListener("mouseleave", function(){
+    aa1.style.backgroundColor = "white"
+    aa1.style.color = "black"
+})
+
+var aa2 = document.querySelector("#aa2");
+aa2.addEventListener("mouseenter", function(){
+    aa2.style.backgroundColor = "black"
+    aa2.style.color = "mediumspringgreen"
+})
+aa2.addEventListener("mouseleave", function(){
+    aa2.style.backgroundColor = "white"
+    aa2.style.color = "black"
+})
+
+var aa3 = document.querySelector("#aa3");
+aa3.addEventListener("mouseenter", function(){
+    aa3.style.backgroundColor = "black"
+    aa3.style.color = "rgb(19, 234, 237)"
+})
+aa3.addEventListener("mouseleave", function(){
+    aa3.style.backgroundColor = "white"
+    aa3.style.color = "black"
+})
+
+var aa4 = document.querySelector("#aa4");
+aa4.addEventListener("mouseenter", function(){
+    aa4.style.backgroundColor = "black"
+    aa4.style.color = "rgb(245, 47, 172)"
+})
+aa4.addEventListener("mouseleave", function(){
+    aa4.style.backgroundColor = "white"
+    aa4.style.color = "black"
+})
+
+var aa5 = document.querySelector("#aa5");
+aa5.addEventListener("mouseenter", function(){
+    aa5.style.backgroundColor = "black"
+    aa5.style.color = "rgb(230, 22, 245)"
+})
+aa5.addEventListener("mouseleave", function(){
+    aa5.style.backgroundColor = "white"
+    aa5.style.color = "black"
+})
+
+var aa6 = document.querySelector("#aa6");
+aa6.addEventListener("mouseenter", function(){
+    aa6.style.backgroundColor = "black"
+    aa6.style.color = "mediumspringgreen"
+})
+aa6.addEventListener("mouseleave", function(){
+    aa6.style.backgroundColor = "white"
+    aa6.style.color = "black"
+})
+
+var aa7 = document.querySelector("#aa7");
+aa7.addEventListener("mouseenter", function(){
+    aa7.style.backgroundColor = "black"
+    aa7.style.color = "yellow"
+})
+aa7.addEventListener("mouseleave", function(){
+    aa7.style.backgroundColor = "white"
+    aa7.style.color = "black"
+})
+
+var aa8 = document.querySelector("#aa8");
+aa8.addEventListener("mouseenter", function(){
+    aa8.style.backgroundColor = "black"
+    aa8.style.color = "rgb(10, 180, 242)"
+})
+aa8.addEventListener("mouseleave", function(){
+    aa8.style.backgroundColor = "white"
+    aa8.style.color = "black"
+})
+
+var aa9 = document.querySelector("#aa9");
+aa9.addEventListener("mouseenter", function(){
+    aa9.style.backgroundColor = "black"
+    aa9.style.color = "rgb(20, 245, 222)"
+})
+aa9.addEventListener("mouseleave", function(){
+    aa9.style.backgroundColor = "white"
+    aa9.style.color = "black"
+})
