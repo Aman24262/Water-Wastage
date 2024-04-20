b4f6b72b571e0d2c7e3a92e27ee732258372ce00
Aman Kumar <152580479+Aman24262@users.noreply.github.com>
2024-04-20 10:26:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,271 @@
+var aa = document.querySelector("#aa");
+aa.addEventListener("mouseenter", function(){
+    aa.style.backgroundColor = "black"
+    aa.style.color = "mediumspringgreen"
+})
+aa.addEventListener("mouseleave", function(){
+    aa.style.backgroundColor = "white"
+    aa.style.color = "black"
+})
+
+var aa1 = document.querySelector("#aa1");
+aa1.addEventListener("mouseenter", function(){
+    aa1.style.backgroundColor = "black"
+    aa1.style.color = "yellow"
+})
+aa1.addEventListener("mouseleave", function(){
+    aa1.style.backgroundColor = "white"
+    aa1.style.color = "black"
+})
+
+var aa01 = document.querySelector("#aa01");
+aa01.addEventListener("mouseenter", function(){
+    aa01.style.backgroundColor = "black"
+    aa01.style.color = "mediumspringgreen"
+})
+aa01.addEventListener("mouseleave", function(){
+    aa01.style.backgroundColor = "white"
+    aa01.style.color = "black"
+})
+
+var aa02 = document.querySelector("#aa02");
+aa02.addEventListener("mouseenter", function(){
+    aa02.style.backgroundColor = "black"
+    aa02.style.color = "yellow"
+})
+aa02.addEventListener("mouseleave", function(){
+    aa02.style.backgroundColor = "white"
+    aa02.style.color = "black"
+})
+
+var aa2 = document.querySelector("#aa2");
+aa2.addEventListener("mouseenter", function(){
+    aa2.style.backgroundColor = "black"
+    aa2.style.color = "mediumspringgreen"
+})
+aa2.addEventListener("mouseleave", function(){
+    aa2.style.backgroundColor = "white"
+    aa2.style.color = "black"
+})
+
+var aa3 = document.querySelector("#aa3");
+aa3.addEventListener("mouseenter", function(){
+    aa3.style.backgroundColor = "black"
+    aa3.style.color = "rgb(19, 234, 237)"
+})
+aa3.addEventListener("mouseleave", function(){
+    aa3.style.backgroundColor = "white"
+    aa3.style.color = "black"
+})
+
+var aa4 = document.querySelector("#aa4");
+aa4.addEventListener("mouseenter", function(){
+    aa4.style.backgroundColor = "black"
+    aa4.style.color = "rgb(245, 47, 172)"
+})
+aa4.addEventListener("mouseleave", function(){
+    aa4.style.backgroundColor = "white"
+    aa4.style.color = "black"
+})
+
+var aa5 = document.querySelector("#aa5");
+aa5.addEventListener("mouseenter", function(){
+    aa5.style.backgroundColor = "black"
+    aa5.style.color = "rgb(230, 22, 245)"
+})
+aa5.addEventListener("mouseleave", function(){
+    aa5.style.backgroundColor = "white"
+    aa5.style.color = "black"
+})
+
+var aa6 = document.querySelector("#aa6");
+aa6.addEventListener("mouseenter", function(){
+    aa6.style.backgroundColor = "black"
+    aa6.style.color = "mediumspringgreen"
+})
+aa6.addEventListener("mouseleave", function(){
+    aa6.style.backgroundColor = "white"
+    aa6.style.color = "black"
+})
+
+var aa7 = document.querySelector("#aa7");
+aa7.addEventListener("mouseenter", function(){
+    aa7.style.backgroundColor = "black"
+    aa7.style.color = "yellow"
+})
+aa7.addEventListener("mouseleave", function(){
+    aa7.style.backgroundColor = "white"
+    aa7.style.color = "black"
+})
+
+var aa8 = document.querySelector("#aa8");
+aa8.addEventListener("mouseenter", function(){
+    aa8.style.backgroundColor = "black"
+    aa8.style.color = "rgb(10, 180, 242)"
+})
+aa8.addEventListener("mouseleave", function(){
+    aa8.style.backgroundColor = "white"
+    aa8.style.color = "black"
+})
+
+var aa9 = document.querySelector("#aa9");
+aa9.addEventListener("mouseenter", function(){
+    aa9.style.backgroundColor = "black"
+    aa9.style.color = "rgb(20, 245, 222)"
+})
+aa9.addEventListener("mouseleave", function(){
+    aa9.style.backgroundColor = "white"
+    aa9.style.color = "black"
+})
+
+var aa10 = document.querySelector("#aa10");
+aa10.addEventListener("mouseenter", function(){
+    aa10.style.backgroundColor = "black"
+    aa10.style.color = "rgb(5, 237, 71)"
+})
+aa10.addEventListener("mouseleave", function(){
+    aa10.style.backgroundColor = "white"
+    aa10.style.color = "black"
+})
+
+var aa11 = document.querySelector("#aa11");
+aa11.addEventListener("mouseenter", function(){
+    aa11.style.backgroundColor = "black"
+    aa11.style.color = "yellow"
+})
+aa11.addEventListener("mouseleave", function(){
+    aa11.style.backgroundColor = "white"
+    aa11.style.color = "black"
+})
+
+var aa12 = document.querySelector("#aa12");
+aa12.addEventListener("mouseenter", function(){
+    aa12.style.backgroundColor = "black"
+    aa12.style.color = "mediumspringgreen"
+})
+aa12.addEventListener("mouseleave", function(){
+    aa12.style.backgroundColor = "white"
+    aa12.style.color = "black"
+})
+
+var aa13 = document.querySelector("#aa13");
+aa13.addEventListener("mouseenter", function(){
+    aa13.style.backgroundColor = "black"
+    aa13.style.color = "mediumspringgreen"
+})
+aa13.addEventListener("mouseleave", function(){
+    aa13.style.backgroundColor = "white"
+    aa13.style.color = "black"
+})
+
+var aa14 = document.querySelector("#aa14");
+aa14.addEventListener("mouseenter", function(){
+    aa14.style.backgroundColor = "black"
+    aa14.style.color = "mediumspringgreen"
+})
+aa14.addEventListener("mouseleave", function(){
+    aa14.style.backgroundColor = "white"
+    aa14.style.color = "black"
+})
+
+var aa15 = document.querySelector("#aa15");
+aa15.addEventListener("mouseenter", function(){
+    aa15.style.backgroundColor = "black"
+    aa15.style.color = "mediumspringgreen"
+})
+aa15.addEventListener("mouseleave", function(){
+    aa15.style.backgroundColor = "white"
+    aa15.style.color = "black"
+})
+
+var aa16 = document.querySelector("#aa16");
+aa16.addEventListener("mouseenter", function(){
+    aa16.style.backgroundColor = "black"
+    aa16.style.color = "mediumspringgreen"
+})
+aa16.addEventListener("mouseleave", function(){
+    aa16.style.backgroundColor = "white"
+    aa16.style.color = "black"
+})
+
+var aa17 = document.querySelector("#aa17");
+aa17.addEventListener("mouseenter", function(){
+    aa17.style.backgroundColor = "black"
+    aa17.style.color = "mediumspringgreen"
+})
+aa17.addEventListener("mouseleave", function(){
+    aa17.style.backgroundColor = "white"
+    aa17.style.color = "black"
+})
+
+var aa18 = document.querySelector("#aa18");
+aa18.addEventListener("mouseenter", function(){
+    aa18.style.backgroundColor = "black"
+    aa18.style.color = "mediumspringgreen"
+})
+aa18.addEventListener("mouseleave", function(){
+    aa18.style.backgroundColor = "white"
+    aa18.style.color = "black"
+})
+
+var aa19 = document.querySelector("#aa19");
+aa19.addEventListener("mouseenter", function(){
+    aa19.style.backgroundColor = "black"
+    aa19.style.color = "mediumspringgreen"
+})
+aa19.addEventListener("mouseleave", function(){
+    aa19.style.backgroundColor = "white"
+    aa19.style.color = "black"
+})
+
+var aa20 = document.querySelector("#aa20");
+aa20.addEventListener("mouseenter", function(){
+    aa20.style.backgroundColor = "black"
+    aa20.style.color = "mediumspringgreen"
+})
+aa20.addEventListener("mouseleave", function(){
+    aa20.style.backgroundColor = "white"
+    aa20.style.color = "black"
+})
+
+var aa21 = document.querySelector("#aa21");
+aa21.addEventListener("mouseenter", function(){
+    aa21.style.backgroundColor = "black"
+    aa21.style.color = "rgb(242, 17, 250)"
+})
+aa21.addEventListener("mouseleave", function(){
+    aa21.style.backgroundColor = "white"
+    aa21.style.color = "black"
+})
+
+var aa22 = document.querySelector("#aa22");
+aa22.addEventListener("mouseenter", function(){
+    aa22.style.backgroundColor = "black"
+    aa22.style.color = "mediumspringgreen"
+})
+aa22.addEventListener("mouseleave", function(){
+    aa22.style.backgroundColor = "white"
+    aa22.style.color = "black"
+})
+
+var aa23 = document.querySelector("#aa23");
+aa23.addEventListener("mouseenter", function(){
+    aa23.style.backgroundColor = "black"
+    aa23.style.color = "mediumspringgreen"
+})
+aa23.addEventListener("mouseleave", function(){
+    aa23.style.backgroundColor = "white"
+    aa23.style.color = "black"
+})
+
+var ax = document.querySelector("#ax");
+ax.addEventListener("mouseenter", function(){
+    ax.style.backgroundColor = "black"
+    ax.style.color = "yellow"
+})
+ax.addEventListener("mouseleave", function(){
+    ax.style.backgroundColor = "white"
+    ax.style.color = "black"
+})
+
+
